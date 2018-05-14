3966352306c364966f95d5412cfc2429db9aa765
leviross <levross@gmail.com>
2018-05-13 21:03:56 -0700

    New BadApi proj, set up main routes and view

--- BadApi/Controllers/HomeController.cs
+++ BadApi/Controllers/HomeController.cs
@@ -2,15 +2,64 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using BadApi.Models;
 using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace BadApi.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private const string _apiBaseUrl = "https://badapi.iqvia.io/api/v1/Tweets";
+        private const string _defaultStartDate = "2016-01-01T00:00:00";
+        private const string _defaultEndDate = "2016-12-12T23:59:59";
+
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var tweets = await GetTweets();
+
+            var viewModel = new ViewModel
+            {
+                Tweets = null,
+            };
+
+            return View("~/Views/Home/Index.cshtml", viewModel);
+        }
+
+        public async Task<List<Tweet>> GetTweets(TweetApi apiContext = null)
+        {
+            if (apiContext == null)
+            {
+                apiContext = new TweetApi
+                {
+                    StartDate = _defaultStartDate,
+                    EndDate = _defaultEndDate
+                };
+            }
+
+            var result = new List<Tweet>();
+
+            using (var client = new HttpClient())
+            {
+                var uri = new Uri($"{_apiBaseUrl}?startDate={apiContext.StartDate}&endDate={apiContext.EndDate}");
+
+                var response = await client.GetAsync(uri);
+                var textResult = await response.Content.ReadAsStringAsync();
+                result = JsonConvert.DeserializeObject<List<Tweet>>(textResult);
+                var me = textResult;
+            }
+
+            return result; 
+        }
+
+        [Route("api/v1/tweets/")]
+        public async Task<List<Tweet>> TweetApi(TweetApi apiContext)
+        {
+            var tweets = await GetTweets(apiContext);
+
+            return tweets;
         }
 
         public ActionResult About()
---
+++ BadApi/Models/ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BadApi.Models
+{
+    public class ViewModel
+    {
+        public IEnumerable<Tweet> Tweets { get; set; }
+    }
+}
